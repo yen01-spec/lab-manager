8d6eed417b948b398e0c51ab5ab0846d95d935ce
yen01-spec <cclye010509@gmail.com>
2026-06-14 02:08:24 +0900

    흰 화면 에러 수정

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,22 @@ import Admin from './pages/Admin'
 import Calendar from './pages/Calendar'
 import Inventory from './pages/Inventory'
 
-function App() {<Routes>
-  <Route path="/" element={<Layout />}>
-    <Route index element={<Home />} />
-    <Route path="calendar" element={<Calendar />} />
-    <Route path="reagents/locations" element={<ReagentLocations />} />
-    <Route path="reagents/list" element={<ReagentList />} />
-    <Route path="items" element={<Items />} />
-    <Route path="requests" element={<Requests />} />
-    <Route path="admin" element={<Admin />} />
-    <Route path="inventory" element={<Inventory />} />
-    <Route path="notices" element={<Notices />} />
-    <Route path="safety" element={<Safety />} />
-  </Route>
-</Routes>}
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="reagents/locations" element={<ReagentLocations />} />
+        <Route path="reagents/list" element={<ReagentList />} />
+        <Route path="items" element={<Items />} />
+        <Route path="requests" element={<Requests />} />
+        <Route path="admin" element={<Admin />} />
+        <Route path="inventory" element={<Inventory />} />
+        <Route path="notices" element={<Notices />} />
+        <Route path="safety" element={<Safety />} />
+      </Route>
+    </Routes>
+  )
+}
 export default App
